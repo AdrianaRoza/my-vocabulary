a5318aaea12b605f6b7ac0c4cad7576c035f4304
Adriana Roza <adriana.roza.aka@gmail.com>
2026-02-25 16:49:20 -0400

    type category

---
+++ src/mock/categories.ts
@@ -0,0 +1,7 @@
+import type { Category } from "../types/category";
+
+export const categories: Category[] = [
+  { id: "body", name: "Body" },
+  { id: "food", name: "Food" },
+  { id: "greeting", name: "Greeting"}
+]
