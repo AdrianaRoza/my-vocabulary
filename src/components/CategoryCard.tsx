111d5045b3697582e33f2fcee22c6476a1c5e42f
Adriana Roza <adriana.roza.aka@gmail.com>
2026-02-25 16:48:55 -0400

    add categoryCard

---
+++ src/components/CategoryCard.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import type { Category } from "../types/category"
+
+interface Props {
+  category: Category
+}
+
+const CategoryCard = ({ category }: Props) => {
+  return (
+    <Link
+      to={`/category/${category.id}`}
+      className="p-6 bg-white shadow rounded-xl text-center hover:scale-105 transition"
+    >
+      {category.name}
+    </Link>
+  )
+}
+
+export default CategoryCard
