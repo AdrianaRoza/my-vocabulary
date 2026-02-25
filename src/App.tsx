a113e98a6d428a5794c71af6d28758ca03f4f16c
Adriana Roza <adriana.roza.aka@gmail.com>
2026-02-25 16:48:07 -0400

    add router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Category from "./pages/Category";
 
 function App() {
-  
-
   return (
-    <>
-      <Home />
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/category/:id" element={<Category />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
